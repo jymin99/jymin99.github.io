7fc8c3923fecd9a625589474b5fca2e2abc6ff46
jymin99 <min10909@naver.com>
2024-06-26 23:55:05 +0900

    빼먹은 commit

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 // import {Link,Element} from 'react-scroll';
 import ListPage from "./ListPage";
-// import AboutMe from './AboutMe'
-// import SkillPage from "./SkillPage";
-// import Portfolio from "./Portfolio";
-// import ContactMe from "./ContactMe";
+
+
 
 const MainPage: React.FC = () => {
     return(
