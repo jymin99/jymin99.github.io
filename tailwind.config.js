16de5b604fa5a5da1ba349a3f8338331868def4f
jymin99 <min10909@naver.com>
2024-06-26 11:12:31 +0900

    "개발환경 세팅"

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        
+      },
+      colors:{
+        'primary': '#F6F0CF',
+        'secondary':'#202020',
+      }
+
+      }
+    },
+  plugins: [],
+}
+
