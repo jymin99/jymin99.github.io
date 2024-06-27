d27a0355706eacc6ac46bdffb3a209d47a3f9579
jymin99 <min10909@naver.com>
2024-06-27 15:17:36 +0900

    listpage issue occur

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,28 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-interface ButtonProps{
+interface ButtonProps {
     title: string;
     navigate_page: string;
 }
-const Button: React.FC<ButtonProps>=({title, navigate_page})=>{
-    const navigate=useNavigate();
-    const onClickMenu=()=>{
+const Button: React.FC<ButtonProps> = ({ title, navigate_page }) => {
+    const navigate = useNavigate();
+    const onClickMenu = () => {
         navigate(navigate_page);
     };
-    return(
-        <button className="bg-transparent text-secondary font-mabeop text-4xl" onClick={onClickMenu}>{title}</button>
+    return (
+        <div className="relative m-5">
+            <button className="relative inline-block z-10 top-2 bg-transparent font-mabeop text-[36px] w-max m-5 whitespace-nowrap ring-transparent" onClick={onClickMenu}>
+                <div className="relative">
+                    <span className="absolute text-white">
+                        {title}
+                    </span>
+                    <span className="absolute text-secondary" style={{top:'0.375rem', left:'0.375rem'}}>
+                        {title}
+                    </span>
+                </div>
+            </button>
+        </div>
     );
 };
 
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 import Button from "../components/Button";
 
-const ListPage: React.FC=()=>{
-    return(
-        <div>
-            {/* 왼쪽 목록 버튼들 */}
-            <div className="flex flex-col flex-start">
-                <Button title="about me" navigate_page="/about"/>
-                <Button title="skills & studing" navigate_page="/skill"/>
-                <Button title="portfolio" navigate_page="/portfolio"/>
-                <Button title="contact" navigate_page="/contact"/>
+const ListPage: React.FC = () => {
+    return (
+        <div className="relative p-10 w-screen h-screen justify-between bg-primary">
+            {/* 왼쪽 목록 버튼-좌측 상단 배치 */}
+            <div className="absolute top-0 left-0 flex flex-col space-y-4">
+                <Button title="about me" navigate_page="/about" />
+                <Button title="skills & studing" navigate_page="/skill" />
+                <Button title="portfolio" navigate_page="/portfolio" />
+                <Button title="contact" navigate_page="/contact" />
+            </div>
+            {/* 이름-우측하단 배치 */}
 
+            <div className="absolute right-0">
+                <div className="relative flex flex-col font-mabeop text-[60px] text-end">
+                    <span className="z-0 absolute text-white" >
+                        <h1>JAE</h1>
+                        <h1>YEON</h1>
+                        <h1>MIN</h1>
+                    </span>
+                    <span className=" z-10 absolute text-secondary " style={{ top: '1rem', left: '1rem' }}>
+                        <h1>JAE</h1>
+                        <h1>YEON</h1>
+                        <h1>MIN</h1>
+                    </span>
+                </div>
             </div>
         </div>
     );
