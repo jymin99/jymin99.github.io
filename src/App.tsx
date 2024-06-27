8063ca240f456085604d94e1b129843592b97a35
jymin99 <min10909@naver.com>
2024-06-27 15:25:36 +0900

    delete mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import {Routes,Route} from "react-router-dom";
 import './index.css';
 import './App.css';
-import MainPage from './pages/MainPage';
+// import MainPage from './pages/MainPage';
 import AboutMe from "./pages/AboutMe";
 import SkillPage from "./pages/SkillPage";
 import Portfolio from "./pages/Portfolio";
 import ContactMe from "./pages/ContactMe";
+import ListPage from "./pages/ListPage";
 
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<ListPage />} />
       <Route path="/about" element={<AboutMe/>}/>
       <Route path="/skill" element={<SkillPage/>}/>
       <Route path="/portfolio" element={<Portfolio/>}/>
