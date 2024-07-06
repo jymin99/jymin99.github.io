636b1a4e8c83ca4d3a165ce9ca90ad9aa7c19409
jymin99 <min10909@naver.com>
2024-07-06 17:38:00 +0900

    portfolio page 화면 작아졌을 때

--- src/components/ModalButton.tsx
+++ src/components/ModalButton.tsx
@@ -15,7 +15,7 @@ const ModalButton: React.FC<ModalButtonProps> = ({ title, type, detail, color })
     return (
         <button 
         // onClick={()=>dispatch(openModal(title))}
-        className={`bg-${color} hover:bg-${color} text-black hover:text-black w-[300px] h-100 p-5 border-transparent hover:border-transparent rounded-[20px] m-5`}
+        className={`bg-${color} hover:bg-${color} text-black hover:text-black w-[300px] h-100 p-5 border-transparent hover:border-transparent rounded-[20px] m-5 max-2xl:m-2`}
         >
             <div className="flex flex-col items-start text-left">
                 <div className="flex flex-row items-center">
